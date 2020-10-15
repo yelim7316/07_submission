771ca5f9291a3f3a6a2425668ff594a54c7ae0bc
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-10-15 16:47:48 +0900

    02

--- main.c
+++ main.c
@@ -5,14 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int flag = 1;
-	int y;
-	while(flag !=0){
-		
-		y=3;
-		flag = 0;
+	int i;
+	
+	for(i=0;i<5;i++)
+	{
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 		
 	}
-	y=4;
+	
+
 	return 0;
 }
